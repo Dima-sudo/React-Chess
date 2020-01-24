876104cea9508a4d4bd05b2fc3c6c29b2262ecad
Dima-sudo <dima.borodinw@gmail.com>
2020-01-24 10:00:32 +0200

    Updates

---
+++ precache-manifest.2ce06ba3f26ef0cc0751d5cdf69fb322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7025adcb2e22d85895541dc6032bc17",
+    "url": "/React-Chess/index.html"
+  },
+  {
+    "revision": "5007b3839d37c8ee60de",
+    "url": "/React-Chess/static/css/main.545ae185.chunk.css"
+  },
+  {
+    "revision": "d2a8915b286fc7dce56a",
+    "url": "/React-Chess/static/js/2.4e9a5c04.chunk.js"
+  },
+  {
+    "revision": "5007b3839d37c8ee60de",
+    "url": "/React-Chess/static/js/main.d5758b83.chunk.js"
+  },
+  {
+    "revision": "ea5437b99a07d86b9bd9",
+    "url": "/React-Chess/static/js/runtime-main.cf57deb6.js"
+  }
+]);
